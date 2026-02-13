a7503b867085f6bd046a5779b2855fdfa962943f
eliasderbewbelay <eliasderbew1@gmail.com>
2026-02-13 15:10:54 +0300

    feat/UI enhancement

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "8000",
-        pathname: "/media/**",
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
     ],
   },
